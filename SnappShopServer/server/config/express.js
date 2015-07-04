92701bdd69c3fcf448349afe832fc7f5adc862fa
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2015-07-04 12:57:05 +0300

    Add initial scaffold of the server.

---
+++ SnappShopServer/server/config/express.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    busboy = require('connect-busboy'),
+    passport = require('passport');
+
+module.exports = function(app, config) {
+    app.use(cookieParser());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(busboy({immediate: false}));
+    app.use(session({secret: 'some secret', resave: true, saveUninitialized: true}));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(express.static(config.rootPath + '/public'));
+    app.use(function(req, res, next) {
+        if (req.session.error) {
+            var msg = req.session.error;
+            req.session.error = undefined;
+            app.locals.errorMessage = msg;
+        }
+        else {
+            app.locals.errorMessage = undefined;
+        }
+
+        next();
+    });
+};
---
+++ SnappShopServer/server/controllers/UsersController.js
@@ -0,0 +1,33 @@
+var encryption = require('../utilities/encryption');
+var users = require('../data/users');
+
+module.exports = {
+    postRegister: function(req, res, next) {
+        var newUserData = req.body;
+
+        if (newUserData.password != newUserData.confirmPassword) {
+            req.session.error = 'Passwords do not match!';
+            res.redirect('/register');
+        }
+        else {
+            newUserData.salt = encryption.generateSalt();
+            newUserData.hashPass = encryption.generateHashedPassword(newUserData.salt, newUserData.password);
+            users.create(newUserData, function(err, user) {
+                if (err) {
+                    console.log('Failed to register new user: ' + err);
+                    return;
+                }
+
+                req.logIn(user, function(err) {
+                    if (err) {
+                        res.status(400);
+                        return res.send({reason: err.toString()}); // TODO
+                    }
+                    else {
+                        res.redirect('/');
+                    }
+                })
+            });
+        }
+    }
+};
